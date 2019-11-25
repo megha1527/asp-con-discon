5c59c51f392d137f24093b670532ba4f69d413ef
Shubham Jain <shubhamjain839@gmail.com>
2019-11-25 03:56:01 -0800

    Add files via upload

---
+++ demo/WebForm1.aspx
@@ -0,0 +1,125 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <style type="text/css">
+        .auto-style1 {
+            width: 133px;
+        }
+        .auto-style2 {
+            width: 411px;
+        }
+        .auto-style3 {
+            width: 133px;
+            height: 30px;
+        }
+        .auto-style4 {
+            width: 411px;
+            height: 30px;
+        }
+    </style>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table>
+            <tr>
+                <td class="auto-style1">Name:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                    <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ErrorMessage="Invalid Name!" ControlToValidate="txtName" ValidationExpression="^[a-zA-Z.'/s]{3,20}"></asp:RegularExpressionValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Class:</td>
+                <td class="auto-style2">
+                    <asp:DropDownList ID="liClass" runat="server">
+                        <asp:ListItem Selected="True">Select Class</asp:ListItem>
+                        <asp:ListItem>MCA</asp:ListItem>
+                        <asp:ListItem>PGDCA</asp:ListItem>
+                        <asp:ListItem>MSc</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Email:</td>
+                <td class="auto-style4">
+                    <asp:TextBox ID="txtEmail" TextMode="Email" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator  ID="RequiredFieldValidator1" runat="server" ErrorMessage="tu Value ni payi bc!" ControlToValidate="txtEmail"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Password:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="txtPass" runat="server" TextMode="Password"></asp:TextBox>
+                    <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Inappropriate Size" ControlToValidate="txtPass" MaximumValue="10" MinimumValue="1"></asp:RangeValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Confirm Password:</td>
+                <td class="auto-style2">
+                    <asp:TextBox ID="txtConfirm" runat="server"></asp:TextBox>
+                    <asp:CompareValidator ID="CompareValidator1" runat="server" ErrorMessage="Password Mismatch" ControlToCompare="txtPass" ControlToValidate="txtConfirm"></asp:CompareValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Gender:</td>
+                <td class="auto-style2">
+                    <asp:RadioButton ID="male" runat="server" GroupName="gender" Text="Male" />
+                    <asp:RadioButton ID="female" runat="server" GroupName="gender" Text="Female" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Hobbies</td>
+                <td class="auto-style2">
+                    <asp:CheckBox ID="ho1" runat="server" Text="Music" />
+                    <asp:CheckBox ID="ho2" runat="server" Text="Sleeping" />
+                    <asp:CheckBox ID="ho3" runat="server" Text="Coding" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:Button ID="subBtn" runat="server" Text="Sumbit" OnClick="subBtn_Click" />
+                </td>
+                <td class="auto-style2">
+                    <asp:Button ID="updBtn" runat="server" Text="Update" OnClick="updBtn_Click" />
+                    <asp:Button ID="delBtn" runat="server" Text="Delete" OnClick="delBtn_Click" />
+                    <asp:Button ID="clrBtn" runat="server" Text="Clear" OnClick="clrBtn_Click" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:apnacon %>" SelectCommand="SELECT * FROM [student]"></asp:SqlDataSource>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="Id" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+                <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Class" HeaderText="Class" SortExpression="Class" />
+                <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                <asp:BoundField DataField="Pass" HeaderText="Pass" SortExpression="Pass" />
+                <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                <asp:BoundField DataField="Hobbies" HeaderText="Hobbies" SortExpression="Hobbies" />
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ demo/WebForm1.aspx.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView1.SelectedRow;
+            txtName.Text = row.Cells[2].Text;
+            liClass.SelectedValue = row.Cells[3].Text;
+            txtEmail.Text = row.Cells[4].Text;
+            string gen = row.Cells[6].Text;
+            if (gen == male.Text)
+                male.Checked = true;
+            else if (gen == female.Text)
+                female.Checked = true;
+            string[] hobAr = row.Cells[7].Text.Split(',');
+            foreach (string ar in hobAr)
+            {
+                if (ar.Trim() == ho1.Text)
+                    ho1.Checked = true;
+                if (ar.Trim() == ho2.Text)
+                    ho2.Checked = true;
+                if (ar.Trim() == ho3.Text)
+                    ho3.Checked = true;
+            }
+        }
+
+        protected void subBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["apnacon"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            string gen = "";
+            if (male.Checked)
+                gen = "Male";
+            else if (female.Checked)
+                gen = "Female";
+            string hob = "";
+            if (ho1.Checked)
+                hob = "Music";
+            if (ho2.Checked)
+                hob += ",Sleeping";
+            if (ho3.Checked)
+                hob += ",Coding";
+            /***************** FOR CONNECTED ENVIRONMENT **************************
+             * *
+             *
+             * 
+             */
+            //cmd.CommandText = "insert into student (Name,Class,Email,Pass,Gender,Hobbies) values ('" + txtName.Text +
+            //                  "','" + liClass.SelectedValue + "','" + txtEmail.Text + "','" + txtPass.Text + "','" +
+            //                  gen + "','" + hob + "')";
+            //cmd.CommandType = CommandType.Text;
+            //cmd.Connection = con;
+            //cmd.ExecuteNonQuery();
+            /***********************
+             *
+             ***************************FOR DISCONNECTED ENVIRONMENT**************************
+             *
+             */
+            SqlDataAdapter da = new SqlDataAdapter("Select * from student",con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "student");
+            DataRow dr = ds.Tables[0].NewRow();
+            dr["Name"] = txtName.Text;
+            dr["Class"] = liClass.SelectedValue;
+            dr["Email"] = txtEmail.Text;
+            dr["Pass"] = txtPass.Text;
+            dr["Gender"] = gen;
+            dr["Hobbies"] = hob;
+            ds.Tables[0].Rows.Add(dr);
+            SqlCommandBuilder scm = new SqlCommandBuilder(da);
+            da.InsertCommand = scm.GetInsertCommand();
+            da.Update(ds,"student");
+            GridView1.DataBind();
+            con.Close();
+            clearAll();
+
+        }
+
+        
+        protected void clrBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm1.aspx");
+        }
+
+        protected void updBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["apnacon"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            string gen = "";
+            if (male.Checked)
+                gen = "Male";
+            else if (female.Checked)
+                gen = "Female";
+            string hob = "";
+            if (ho1.Checked)
+                hob = "Music";
+            if (ho2.Checked)
+                hob += ",Sleeping";
+            if (ho3.Checked)
+                hob += ",Coding";
+            /***************** FOR CONNECTED ENVIRONMENT **************************
+             * *
+             *
+             * 
+             */
+
+
+            //cmd.CommandText = "update student set Name='" + txtName.Text +
+            //                  "',Class='" + liClass.SelectedValue + "',Email='" + txtEmail.Text + "',Pass='" +
+            //                  txtPass.Text + "',Gender='" +
+            //                  gen + "',Hobbies='" + hob + "' where Id = "+GridView1.SelectedRow.Cells[1].Text ;
+            ////cmd.CommandType = CommandType.Text;
+            //cmd.Connection = con;
+            //cmd.ExecuteNonQuery();
+
+            /***********************
+            *
+            ***************************FOR DISCONNECTED ENVIRONMENT**************************
+            *
+            */
+            SqlDataAdapter da = new SqlDataAdapter("Select * from student", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "student");
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Id"] = GridView1.SelectedRow.Cells[1].Text;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Name"] = txtName.Text;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Class"] = liClass.SelectedValue;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Email"] = txtEmail.Text;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Pass"] = txtPass.Text;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Gender"] = gen;
+            ds.Tables[0].Rows[GridView1.SelectedIndex]["Hobbies"] = hob;
+            SqlCommandBuilder scm = new SqlCommandBuilder(da);
+            da.UpdateCommand = scm.GetUpdateCommand();
+            da.Update(ds, "student");
+            GridView1.DataBind();
+            con.Close();
+            clearAll();
+
+        }
+        protected void clearAll()
+        {
+            txtName.Text = "";
+            txtConfirm.Text = "";
+            txtEmail.Text = "";
+            txtPass.Text = "";
+            liClass.SelectedValue = "Select Class";
+            male.Checked = false;
+            female.Checked = false;
+            ho1.Checked = false;
+            ho2.Checked = false;
+            ho3.Checked = false;
+        }
+
+        protected void delBtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["apnacon"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            /***************** FOR CONNECTED ENVIRONMENT **************************
+             * *
+             *
+             * 
+             */
+
+
+            //cmd.Connection = con;
+            //cmd.CommandText = "delete from student where Id = " + id;
+            //cmd.ExecuteNonQuery();
+
+            /***********************
+            *
+            ***************************FOR DISCONNECTED ENVIRONMENT**************************
+            *
+            */
+            SqlDataAdapter da = new SqlDataAdapter("Select * from student", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "student");
+            DataRow dr = ds.Tables[0].Rows[GridView1.SelectedIndex];
+            dr.Delete();
+            SqlCommandBuilder scm = new SqlCommandBuilder(da);
+            da.DeleteCommand = scm.GetDeleteCommand();
+            da.Update(ds, "student");
+            con.Close();
+            GridView1.DataBind();
+            clearAll();
+        }
+    }
+}
